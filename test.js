e2677c4bce8bdfa64d25b77ec9e20b7018b7bda8
Ryan Ozimek <cozimek@picnet.net>
2024-04-24 10:06:17 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,20 @@
-// test.js
 const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
 
 async function exampleTest() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  // Set up Chrome options for headless execution
+  let chromeOptions = new chrome.Options();
+  chromeOptions.addArguments('--headless');             // Runs Chrome in headless mode.
+  chromeOptions.addArguments('--no-sandbox');           // Disables the sandbox for all software renderer processes.
+  chromeOptions.addArguments('--disable-dev-shm-usage');// Resolves limited resource problems.
+  chromeOptions.addArguments('--disable-gpu');          // Disables GPU hardware acceleration.
+
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(chromeOptions)
+    .build();
+
   try {
     // Navigate to Soapbox Engage
     await driver.get('https://demo.secure.nonprofitsoapbox.com');
